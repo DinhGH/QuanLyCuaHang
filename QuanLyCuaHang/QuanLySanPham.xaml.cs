03bf17fa6cdf8e13ad18b63a0ec1b3e1a15bfb4e
Huyen <trandieuhuyen@dtu.edu.vn>
2026-02-26 14:48:27 +0700

    Update QuanLySanPham DataGrid mapping to products table

--- QuanLyCuaHang/QuanLySanPham.xaml.cs
+++ QuanLyCuaHang/QuanLySanPham.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +24,57 @@ namespace QuanLyCuaHang
         public QuanLySanPham()
         {
             InitializeComponent();
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            try
+            {
+                string dbPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\QLCHDB.mdf"));
+                string connectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True;Connect Timeout=30;MultipleActiveResultSets=True;";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                using (SqlCommand command = new SqlCommand("SELECT id, name, price, quantity, unit, description, created_at FROM dbo.products", connection))
+                {
+                    connection.Open();
+
+                    var products = new List<ProductGridItem>();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            products.Add(new ProductGridItem
+                            {
+                                id = reader.GetInt32(reader.GetOrdinal("id")),
+                                name = reader.GetString(reader.GetOrdinal("name")),
+                                price = reader.GetDecimal(reader.GetOrdinal("price")),
+                                quantity = reader.GetInt32(reader.GetOrdinal("quantity")),
+                                unit = reader.IsDBNull(reader.GetOrdinal("unit")) ? null : reader.GetString(reader.GetOrdinal("unit")),
+                                description = reader.IsDBNull(reader.GetOrdinal("description")) ? null : reader.GetString(reader.GetOrdinal("description")),
+                                created_at = reader.IsDBNull(reader.GetOrdinal("created_at")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("created_at"))
+                            });
+                        }
+                    }
+
+                    dgDanhSach.ItemsSource = products;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Không thể tải dữ liệu sản phẩm từ bảng products.\n" + ex.Message, "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private class ProductGridItem
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public decimal price { get; set; }
+            public int quantity { get; set; }
+            public string unit { get; set; }
+            public string description { get; set; }
+            public DateTime? created_at { get; set; }
         }
     }
 }
